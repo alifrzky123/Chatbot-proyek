c0eb38aecb5afbe8d297598925218939baa91b83
DESKTOP-TPOLUJ6\Alif <alifrzky78@gmail.com>
2022-08-11 17:40:17 +0700

    merubah struktur project

--- app.py
+++ app.py
@@ -14,18 +14,19 @@ words = pickle.load(open('texts.pkl','rb'))
 classes = pickle.load(open('labels.pkl','rb'))
 
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
+    # tokenize patternya - bagi kata menjadi sebuah array
     sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
+    # stem tiap kata - membuat bentuk pendek untuk kata
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+# mengembalikan array pada bag of words: 0 atau 1 untuk setiap kata yang muncul dalam sebuah kalimat
 
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
+    # tokenize patterns
     sentence_words = clean_up_sentence(sentence)
     # bag of words - matrix of N words, vocabulary matrix
+    # bag of words - matrix dari N kata, matriks vocabulary nya
     bag = [0]*len(words)  
     for s in sentence_words:
         for i,w in enumerate(words):
@@ -33,7 +34,7 @@ def bow(sentence, words, show_details=True):
                 # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
+                    print ("ditemukan pada bag: %s" % w)
     return(np.array(bag))
 
 def predict_class(sentence, model):
@@ -71,10 +72,6 @@ app.static_folder = 'static'
 
 @app.route("/")
 def home():
-    return render_template("home.html")
-
-@app.route("/inter")
-def inter():
     return render_template("index.html")
 
 @app.route("/get")
--- training.py
+++ training.py
@@ -21,60 +21,60 @@ intents = json.loads(data_file)
 for intent in intents['intents']:
     for pattern in intent['patterns']:
 
-        #tokenize each word
+        #tokenize tiap kata
         w = nltk.word_tokenize(pattern)
         words.extend(w)
         #add documents in the corpus
         documents.append((w, intent['tag']))
 
-        # add to our classes list
+        # ditambahkan ke list kelas/intents
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# lemmaztize and lower each word and remove duplicates
+# lemmaztize dan mengecilkan tiap kata dan menghapus kata yang sama
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
 # sort classes
 classes = sorted(list(set(classes)))
-# documents = combination between patterns and intents
-print (len(documents), "documents")
+# documents = umlah dokumen yang telah digabungkan antara patterns dengan intents
+print (len(documents), "Jumlah dokumen yang telah digabungkan antara patterns dengan intents")
 # classes = intents
-print (len(classes), "classes", classes)
-# words = all words, vocabulary
-print (len(words), "unique lemmatized words", words)
+print (len(classes), "Jumlah Intents", classes)
+# words = Semua kata
+print (len(words), "Kata yang sudah di processing", words)
 
 
 pickle.dump(words,open('texts.pkl','wb'))
 pickle.dump(classes,open('labels.pkl','wb'))
 
-# create our training data
+# Membuat data training
 training = []
-# create an empty array for our output
+# Membuat array kosong untuk output
 output_empty = [0] * len(classes)
-# training set, bag of words for each sentence
+# training set, bag of words untuk tiap kata
 for doc in documents:
-    # initialize our bag of words
+    # inisiasi bag of words
     bag = []
-    # list of tokenized words for the pattern
+    # list tiap kata yang sudah di tokenisasi
     pattern_words = doc[0]
-    # lemmatize each word - create base word, in attempt to represent related words
+    # lemmatize setiap kata, membuat kata dasarnya dengan tujuan untuk diberikan pada kata yang berhubungan
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # create our bag of words array with 1, if word match found in current pattern
+    # membuat bag of words dengan array 1 jika kata yang ditemukan sama dengan pattern saat ini
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
     
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    # outputnya adalah '0' untuk tiap tag dan '1' untuk tag saat ini (untuk setiap pattern)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
     
     training.append([bag, output_row])
-# shuffle our features and turn into np.array
+# Acak tiap kata dan ubah ke bentuk array
 random.shuffle(training)
 training = np.array(training)
-# create train and test lists. X - patterns, Y - intents
+# membuat data train dan test, X untuk patterns dan Y untuk intents
 train_x = list(training[:,0])
 train_y = list(training[:,1])
-print("Training data created")
+print("Data Training Telah Dibuat")
 
 
 # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
